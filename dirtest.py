9e1c30d93061ef51de8a886963253e84fa3f6fae
KaiTouKID1412007 <118148573+KaiTouKID1412007@users.noreply.github.com>
2022-11-21 21:08:37 +0800

    Update dirtest.py

--- dirtest.py
+++ dirtest.py
@@ -17,23 +17,23 @@ import shutil
 #Get the current directory with getcwd first 先用getcwd取得当前目录
 filewd=os.getcwd()
 print(filewd)
-#使用相对路径创建目录 Use relative paths to create directories
-#path1为要创建目录的目录路径 Is the directory path to which the directory is to be created
+# Use relative paths to create directories
+# Is the directory path to which the directory is to be created
 path1="testcreate"
-#调用OS模块的chdir方法修改程序工作目录 Call the chdir method of the OS module to modify the program working directory
+#Call the chdir method of the OS module to modify the program working directory
 os.chdir("E:/")
-#调用OS模块的mkdir方法创建目录 Call the mkdir method of the OS module to create the directory
+# Call the mkdir method of the OS module to create the directory
 os.mkdir(path1)
-print('新文件夹创建成功！New DIR created successfully!')  #New DIR created successfully!
+print('New DIR created successfully!')  #New DIR created successfully!
 f=open('E:/testcreate/testp.txt','w')
 f.write('Hello World!')
 f.close()
 
 # List files and subdirectories
 
-#获取指定目录下的所有文件 Gets all files in the specified directory
+# Gets all files in the specified directory
 listdir1=os.listdir(path1)
-print(" 目录下包含的文件和目录：",listdir1)
+print(" all files in the specified directory：",listdir1)
 
 # Move file or subdirectory to another location
 
@@ -48,6 +48,6 @@ print("After moving Where the file is:")
 path3="E:/testcreate"
 # Invoke the rmdir method of the os module to delete the directory
 os.rmdir(path3)
-print('文件夹删除成功！DIR Remove successfully!')  #New DIR created successfully!
+print('！DIR Remove successfully!')  #New DIR created successfully!
 
 
