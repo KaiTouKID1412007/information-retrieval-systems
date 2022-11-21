e6d64f1b4973d54870b7c007b16999c589ee5013
KaiTouKID1412007 <118148573+KaiTouKID1412007@users.noreply.github.com>
2022-11-21 21:06:49 +0800

    Update binaryfiletest.py

--- binaryfiletest.py
+++ binaryfiletest.py
@@ -12,12 +12,12 @@ import shutil
 
 studentinfo1={'name':'WeiYirui','sex':'Female','Country':'China','City':'Shanghai'}
 # Create /open file
-with shelve.open('test02.bin') as fp: #创建或打开二进制文件 Create or open a binary file
-    fp['1']=studentinfo1  #以字典形式把数据写入文件 Write data to a file in dictionary form
+with shelve.open('test02.bin') as fp: # Create or open a binary file
+    fp['1']=studentinfo1  # Write data to a file in dictionary form
     # Readfile (returns file content)
     print(fp['1'])
 
-#Get the current directory with getcwd first 先用getcwd取得当前目录
+#Get the current directory with getcwd first 
 shutil.copy("test02.bin.dat","test02.bin")
 shutil.move("test02.bin.dat","movetest02.bin")
 # Delete file
